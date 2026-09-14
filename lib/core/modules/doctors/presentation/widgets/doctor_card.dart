8a4d36fa5f19a920f97789c52039556eb5f79a03
unknown <amr_elngm6@yahoo.com>
2026-09-14 09:48:02 +0300

    Fixed Some issues

--- lib/core/modules/doctors/presentation/widgets/doctor_card.dart
+++ lib/core/modules/doctors/presentation/widgets/doctor_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:bedaya2/core/theme/colors.dart';
